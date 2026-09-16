168d7dadf49728ceb88d167c21748fbe36271448
pranajaa <hitanapranaja06@gmail.com>
2026-09-16 10:41:33 +0700

    docs: memperbarui kuis

---
+++ latihan/04_jenis_segitiga.py
@@ -0,0 +1,13 @@
+a = float(input("Sisi a: "))
+b = float(input("Sisi b: "))
+c = float(input("Sisi c: "))
+
+if a + b > c and a + c > b and b + c > a:
+    if a == b == c:
+        print("Segitiga sama sisi")
+    elif a == b or a == c or b == c:
+        print("Segitiga sama kaki")
+    else:
+        print("Segitiga sembarang")
+else:
+    print("Ketiga sisi tidak membentuk segitiga")
